9de1f05a127f72ff60ff2fa65c541d1cd40886a9
y-uchi <bonbonheart@gmail.com>
2024-03-20 21:06:41 +0900

    結合テストの実装

---
+++ spec/support/sign_in_support.rb
@@ -0,0 +1,11 @@
+module SignInSupport
+  def sign_in(user)
+    visit root_path
+    expect(page).to have_current_path(new_user_session_path)
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_on 'Log in'
+    sleep 1
+    expect(page).to have_current_path(root_path)
+  end
+end
